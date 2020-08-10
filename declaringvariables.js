a46917bdd035d38a8d9a94444159e3067df30cc0
shubhamkamble <kshub44kamble@gmail.com>
2020-08-10 18:24:27 +0530

    added: Declaring Variables

---
+++ declaringvariables.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Declare the variable message and assign the value 'Hello' to it.
+var message = 'Hello';
+
+// Print message
+console.log(message);
+
+// Assign a new value to the variable "message"
+message = 'I am learning Javascript';
+
+console.log(message);
