8e98dde13bab9c452e563285c155ce7929d9ae12
shubhamkamble <kshub44kamble@gmail.com>
2020-08-12 16:22:23 +0530

    added: Arithmetic Operators

---
+++ 04arithmetic.js
@@ -0,0 +1,24 @@
+// Arithmetic operator
+console.log(5 + 4);
+console.log(9 - 5);
+console.log(3 * 4);
+console.log(7 / 2);
+
+// Mod operator
+console.log(7 % 2);
+console.log(12.3 % 5);
+
+// Unary operator
+let value = 2;
+value++;
+
+console.log(value);
+
+++value;
+console.log(value);
+
+--value;
+console.log(value);
+
+value--;
+console.log(value);
